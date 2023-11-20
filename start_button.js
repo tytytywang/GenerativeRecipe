6081b58a82b0371141bf9f9f36e8043b95e7b793
mohanchi <chimohan@outlook.com>
2023-11-20 02:53:07 -0500

    Add communication with the server

--- start_button.js
+++ start_button.js
@@ -25,10 +25,22 @@
 async function startChat() {
     // Get the user input
     const userInput = document.getElementById('textInput').value;
+    console.log('@@@@@@@userInput:' + userInput);
+    $.ajax({
+        url: 'http://127.0.0.1:8088/GetRecipeCategories',
+        type: 'POST',
+        data: { 'input': userInput },
+        contentType: 'application/x-www-form-urlencoded',
+        success: function (data) {
+            console.log('data: ' + data);
+            if (data)
+                alert('categories: ' + data);
+        }
+    });
 
     // Redirect to a new page
-    window.location.href = 'explosive_graph.html';
+    //window.location.href = 'explosive_graph.html';
 
     // Optionally, you can also send the user input to the new page via localStorage
-    localStorage.setItem('userInput', userInput);
+    //localStorage.setItem('userInput', userInput);
 }
